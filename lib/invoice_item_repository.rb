ab5e6ebd0674a45a842d6a43c14527305b286143
HoffsMH <hecker.matthew@gmail.com>
2015-07-28 22:48:54 -0600

    added invoice_item_repo

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,27 @@
+require 'pry'
+require_relative '../lib/loader.rb'
+require_relative '../lib/invoice_item.rb'
+
+class InvoiceItemRepository
+  attr_accessor :invoice_items
+  def initialize(filename='./data/invoice_items.csv')
+    @loader = Loader.new
+    loaded_csvs = @loader.load_csv(filename)
+    @invoice_items = populate_invoice_items(loaded_csvs)
+  end
+
+  def add_invoice_item(record)
+    InvoiceItem.new(record)
+  end
+
+  def populate_invoice_items(loaded_csvs)
+    invoice_items = {}
+    loaded_csvs.each do |invoice_item|
+      id = invoice_item.first
+      record = invoice_item.last
+      invoice_items[id] = add_invoice_item(record)
+    end
+    invoice_items
+  end
+
+end
---
+++ test/invoice_item_repository_test.rb
@@ -0,0 +1,37 @@
+require_relative 'test_helper.rb'
+require_relative '../lib/invoice_item_repository'
+
+class InvoiceItemRepositoryTest < Minitest::Test
+  
+  def setup
+    @invoice_item_repository = InvoiceItemRepository.new('./fixtures/invoice_items.csv')
+  end  
+  def test_make_sure_we_can_instantiate
+    assert @invoice_item_repository.class == InvoiceItemRepository
+  end
+  def test_we_can_make_instances_of_Invoice_Item
+    
+    invoice_item_record = {:item_id => '2310',
+                         :invoice_id => '5603',
+                         :quantity => "2",
+                         :unit_price => "12412",
+                        :created_at => "sometime",
+                        :updated_at => "someothertime"}
+    invoice_item = @invoice_item_repository.add_invoice_item(invoice_item_record)
+    
+    expected = "12412"
+    result = invoice_item.unit_price
+    
+    assert_equal expected,  result
+  end
+  def test_we_can_populate_invoices
+    assert @invoice_item_repository.invoice_items.length > 20
+  end
+  def test_we_can_access_a_invoices_info_from_the_invoice_repo_class
+    
+    expected = 1830
+    result = @invoice_item_repository.invoice_items[10].item_id
+    
+    assert_equal expected, result
+  end
+end
