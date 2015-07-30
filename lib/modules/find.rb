5b088e1a09b1e8c8c182a16fad4dfb4465cd4952
HoffsMH <hecker.matthew@gmail.com>
2015-07-30 10:15:57 -0600

    added find_all

--- lib/modules/find.rb
+++ lib/modules/find.rb
@@ -2,13 +2,23 @@ require 'pry'
 module Find
 
   def find_by(x, match)
+    found = find_all_by(x, match)
+    if found.first.nil?
+      false
+    else
+      found[0]
+    end
+  end
+  def find_all_by(x, match)
+    x = x.to_sym
+    match = match.to_s.downcase
     if @records[1].respond_to?(x)
       found = @records.select do |id, record|
-        match ==  record.send(x)
+        match == record.send(x).to_s.downcase
       end
       return found.to_a
     else
-      "not a valid column"
+      []
     end
   end
 end
--- test/find_test.rb
+++ test/find_test.rb
@@ -7,29 +7,54 @@ require_relative '../lib/repos/customer_repository'
 
 
 class ListTest < Minitest::Test     
-  def test_we_can_determine_if_our_records_have_x_type
+  def test_we_can_determine_if_our_records_have_x_type_find_by
     @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
     
     
     result = @cust_repo.find_by(:last_name, "Sawayn")
     
-    assert_equal(true, !!result[0].last.last_name)
+    assert_equal(true, !!result.last.last_name)
   end
-  def test_we_can_determine_if_a_record_is_on_the_list
+  def test_we_can_determine_if_our_records_have_x_type_find_by_false
+    @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
+    
+    
+    result = @cust_repo.find_by(:dududududu, "Sawayn")
+    
+    assert_equal(false, !!result)
+  end
+  def test_we_can_determine_if_a_record_is_on_the_list_find_by
     @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
     
     expected = @cust_repo.customers[26].last_name
     result = @cust_repo.find_by(:last_name, "Sawayn")
     
-    assert_equal(expected, result[0].last.last_name)
+    assert_equal(expected, result.last.last_name)
     
   end
-  def test_we_return_an_empty_array_if_the_record_doesnt_exist
+  def test_we_get_back_false_if_record_doesnt_exist_find_by
+    @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
+    
+    
+    result = @cust_repo.find_by(:last_name, "Sawadfyn")
+    
+    assert_equal(false, result)
+    
+  end
+  def test_we_return_an_empty_array_if_the_record_doesnt_exist_find_all
     @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
     
     expected = []
-    result = @cust_repo.find_by(:last_name, "Sasdsn")
+    result = @cust_repo.find_all_by(:last_name, "Sasdsn")
     
     assert_equal(expected, result)
+  end
+  def test_we_return_an_array_of_results_if_we_find_them_find_all
+    @cust_repo = CustomerRepository.new('./fixtures/customers.csv')
+    
+    expected = @cust_repo.customers[26].last_name
+    result = @cust_repo.find_all_by(:last_name, "Sawayn")
+    
+    assert_equal(expected, result[0].last.last_name)
   end
 end
